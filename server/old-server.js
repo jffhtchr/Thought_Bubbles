f53aa8b4839a7dfee3dd20558fc53d1492a709c5
Jeff Hatcher <jeffhatcher@Jeffs-MacBook-Pro.local>
2018-03-12 17:07:20 -0400

    fix: server fix

---
+++ server/old-server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const db = require('./db'); 
+const socketio = require('socket.io');
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+const server = app.listen(PORT, function () {
+      console.log(`Listening on port ${PORT}`);
+});
+    
+var io = socketio(server);
+
+io.on('connection', function(socket){
+    socket.on('SEND_MESSAGE', function(data){
+      io.emit('RECIEVE_MESSAGE', data)
+  })
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error');
+  });
--- server/server.js
+++ server/server.js
@@ -1,29 +1,56 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const db = require('./db'); 
-const socketio = require('socket.io');
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const db = require('./db')
+const socketio = require('socket.io')
+module.exports = app
 
-app.use(express.static(path.join(__dirname, '../public')))
+const createApp = () => {
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
 
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-})
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
 
-const server = app.listen(PORT, function () {
-      console.log(`Listening on port ${PORT}`);
-});
-    
-var io = socketio(server);
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
 
-io.on('connection', function(socket){
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+
+  // set up our socket control center
+  const io = socketio(server)
+
+  io.on('connection', function(socket){
     socket.on('SEND_MESSAGE', function(data){
       io.emit('RECIEVE_MESSAGE', data)
   })
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || 'Internal server error');
-  });
+}
+if (require.main === module) {
+    createApp()
+    startListening()
+} else {
+  createApp()
+}
